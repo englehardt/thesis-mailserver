a1a262c801f23e9d5eb0208954ba4b2ababa6f89
Jeffrey Han <itdelatrisu@gmail.com>
2017-02-06 22:08:01 -0500

    Better organize LinkExtractor output.

--- src/itdelatrisu/mailserver/LinkExtractor.java
+++ src/itdelatrisu/mailserver/LinkExtractor.java
@@ -13,24 +13,51 @@ import javax.mail.Part;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Link extractor.
  */
 public class LinkExtractor {
-	private static final Logger logger = LoggerFactory.getLogger(LinkExtractor.class);
+	/** Image data. */
+	public class Image {
+		/** Source URL. */
+		public final String url;
+
+		/** Dimension attributes. */
+		public final String width, height;
+
+		/** Creates a new image. */
+		public Image(String url, String width, String height) {
+			this.url = url;
+			this.width = width;
+			this.height = height;
+		}
+	}
 
 	/** All links. */
-	private final List<String> links;
+	private final List<String> links = new ArrayList<String>();
+
+	/** All inline images (e.g. in 'img' tags). */
+	private final List<Image> inlineImages = new ArrayList<Image>();
+
+	/** All images in inline CSS (e.g. in 'url()' values). */
+	private final List<String> inlineCssImages = new ArrayList<String>();
+
+	/** All imports (e.g. external stylesheets). */
+	private final List<String> imports = new ArrayList<String>();
+
+	/** All inline links (e.g. in 'a' tags). */
+	private final List<String> inlineLinks = new ArrayList<String>();
+
+	/** All other media (e.g. with 'src' keys, but not 'img' tags). */
+	private final List<String> media = new ArrayList<String>();
 
 	/**
 	 * Extracts links from an HTML body.
 	 * @param html the HTML body
 	 */
 	public LinkExtractor(String html) {
-		this.links = extractLinksFromHtml(html);
+		extractLinksFromHtml(html);
 	}
 
 	/**
@@ -38,63 +65,80 @@ public class LinkExtractor {
 	 * @param message the MIME message
 	 */
 	public LinkExtractor(Part message) throws MessagingException, IOException {
-		this.links = extractLinks(message);
+		extractLinksFromMessage(message);
 	}
 
 	/** Returns all extracted links. */
-	public List<String> getLinks() { return links; }
+	public List<String> getAllLinks() { return links; }
 
-	/** Returns all links contained in the HTML sectionsn of a MIME message. */
-	private List<String> extractLinks(Part message) throws MessagingException, IOException {
+	/** Returns all inline images (e.g. in 'img' tags). */
+	public List<Image> getInlineImages() { return inlineImages; }
+
+	/** Returns all images in inline CSS (e.g. in 'url()' values). */
+	public List<String> getInlineCssImages() { return inlineCssImages; }
+
+	/** Returns all imports (e.g. external stylesheets). */
+	public List<String> getImports() { return imports; }
+
+	/** Returns all inline links (e.g. in 'a' tags). */
+	public List<String> getInlineLinks() { return inlineLinks; }
+
+	/** Returns all other media (e.g. with 'src' keys, but not 'img' tags). */
+	public List<String> getMedia() { return media; }
+
+	/** Finds all links contained in the HTML sections of a MIME message. */
+	private void extractLinksFromMessage(Part message) throws MessagingException, IOException {
 		// html: parse and return
-		if (message.getContentType().startsWith("text/html"))
-			return extractLinksFromHtml((String) message.getContent());
+		if (message.getContentType().startsWith("text/html")) {
+			extractLinksFromHtml((String) message.getContent());
+			return;
+		}
 
 		// multipart: recursively check parts for html
 		if (message.getContentType().startsWith("multipart/")) {
-			List<String> list = new ArrayList<String>();
 			Multipart multipart = (Multipart) message.getContent();
 			for (int i = 0; i < multipart.getCount(); i++)
-				list.addAll(extractLinks(multipart.getBodyPart(i)));
-			return list;
+				extractLinksFromMessage(multipart.getBodyPart(i));
+			return;
 		}
-
-		// nothing
-		return new ArrayList<String>(0);
 	}
 
-	/** Returns all links contained in an HTML body. */
-	private List<String> extractLinksFromHtml(String html) {
-		List<String> list = new ArrayList<String>();
-
+	/** Finds all links contained in an HTML body. */
+	private void extractLinksFromHtml(String html) {
 		// parse document
 		Document doc = Jsoup.parse(html);
 
 		// media
 		for (Element src : doc.select("[src]")) {
-			list.add(src.attr("abs:src"));
-
-			if (src.tagName().equals("img"))
-				logger.info("image with size {}x{}: {}", src.attr("width"), src.attr("height"), src.attr("abs:src"));
+			String url = src.attr("abs:src");
+			if (src.tagName().equals("img")) {
+				String width = src.attr("width").trim(), height = src.attr("height").trim();
+				inlineImages.add(new Image(url, width, height));
+			} else
+				media.add(url);
+			links.add(url);
 		}
 
 		// imports
 		for (Element link : doc.select("link[href]")) {
-			list.add(link.attr("abs:href"));
-
-			if (link.attr("abs:href").endsWith(".css"))
-				logger.info("external stylesheet: {}", link.attr("abs:href"));
+			String url = link.attr("abs:href");
+			imports.add(url);
+			links.add(url);
 		}
 
 		// links
-		for (Element link : doc.select("a[href]"))
-			list.add(link.attr("abs:href"));
+		for (Element link : doc.select("a[href]")) {
+			String url = link.attr("abs:href");
+			inlineLinks.add(url);
+			links.add(url);
+		}
 
 		// css
-		for (Element css : doc.select("style"))
-			list.addAll(extractLinksFromCSS(css.data()));
-
-		return list;
+		for (Element css : doc.select("style")) {
+			List<String> cssLinks = extractLinksFromCSS(css.data());
+			inlineCssImages.addAll(cssLinks);
+			links.addAll(cssLinks);
+		}
 	}
 
 	/** Returns all links contained in a CSS body. */
